dcecf96a5ed692dcdb6a328a3019a0d0f63b81e8
CeciliaDiesch <ceciliadiesch@yahoo.com>
2025-09-26 13:05:06 +0200

    feat: signin dialog

--- src/app/backdrop-component/backdrop-component.ts
+++ src/app/backdrop-component/backdrop-component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogSigninComponent } from '../dialogs/dialog.signin-component/dialog.signin-component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-backdrop-component',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './backdrop-component.html',
   styleUrl: './backdrop-component.scss'
 })
 export class BackdropComponent {
+
+    constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogSigninComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 
 }
