99c6a78c64142f8070580d820e3eeb974a984a16
Mark Corzilius <marccorzprivate@gmail.com>
2025-09-26 20:26:27 +0200

    fix: centered  main-content | perf: Conditionally displays UI elements like the Sign Up link and footer based on the current route.

--- src/app/backdrop-component/backdrop-component.ts
+++ src/app/backdrop-component/backdrop-component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-backdrop-component',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './backdrop-component.html',
-  styleUrl: './backdrop-component.scss'
+  styleUrl: './backdrop-component.scss',
 })
 export class BackdropComponent {
+  private footerVisibleOnRoutes = [
+    '/login',
+    '/signin',
+    '/forgot-password',
+    '/reset-password',
+    '/avatar-select',
+  ];
+
+  constructor(public router: Router) {}
+
+  get showFooter(): boolean {
+    return this.footerVisibleOnRoutes.some(route => this.router.url.includes(route))
+  }
+
 
 }
